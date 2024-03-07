bfbb362bea456904e65d60574e9a763799d43272
june <dydrkfl078@naver.com>
2024-03-08 03:15:54 +0900

    feat/add_sign_in_page
    중복 코드들을 common.css ,common.js로 분리,
    로그인 페이지 구현

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 const badgeEl = document.querySelector("header .badges");
 
 window.addEventListener(
@@ -159,6 +142,3 @@ new Swiper(".awards .swiper", {
     nextEl: ".awards .swiper-next",
   },
 });
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
